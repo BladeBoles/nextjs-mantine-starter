e425900556b7fde584c0d07f9148c48b36d9d2ce
Blade Boles <bladerboles@gmail.com>
2024-05-01 22:43:19 -0600

    feat: create basic todo list

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,74 @@
+"use client";
+import { Textarea, List, Button } from "@mantine/core";
+import { useState } from "react";
+
+interface ResetTodosButtonProps {
+  resetTodos: () => void;
+}
+interface AddTodoInputProps {
+  value: string;
+  setValue: (value: string) => void;
+}
+interface AddTodoButtonProps {
+  addTodo: () => void;
+}
+function AddTodoInput({ value, setValue }: AddTodoInputProps) {
+  return (
+    <Textarea
+      onChange={(event) => setValue(event.target.value)}
+      value={value}
+    ></Textarea>
+  );
+}
+function AddTodoButton({ addTodo }: AddTodoButtonProps) {
+  return <Button onClick={addTodo}>Add Todo!</Button>;
+}
+function ResetTodosButton({ resetTodos }: ResetTodosButtonProps) {
+  return (
+    <Button color="gray" onClick={resetTodos}>
+      Reset Todos :(
+    </Button>
+  );
+}
+function TodoList({
+  todos = [],
+  removeTodo,
+}: {
+  todos: string[];
+  removeTodo: (index: number) => void;
+}) {
+  return (
+    <List>
+      {todos.map((todo, index) => (
+        <List.Item key={index}>
+          {todo}
+
+          <Button color="red" onClick={() => removeTodo(index)}>
+            Delete!
+          </Button>
+        </List.Item>
+      ))}
+    </List>
+  );
+}
 export default function HomePage() {
-  return <div>Home page</div>;
+  const [todos, setTodos] = useState<string[]>([]);
+  const [value, setValue] = useState("");
+
+  const addTodo = () => {
+    setTodos([...todos, value]);
+    setValue("");
+  };
+  const removeTodo = (todoIndex: number) =>
+    setTodos(todos.filter((_, index) => index !== todoIndex));
+
+  const resetTodos = () => setTodos([]);
+  return (
+    <>
+      <AddTodoInput value={value} setValue={setValue}></AddTodoInput>
+      <AddTodoButton addTodo={addTodo}></AddTodoButton>
+      <TodoList todos={todos} removeTodo={removeTodo}></TodoList>
+      <ResetTodosButton resetTodos={resetTodos}></ResetTodosButton>
+    </>
+  );
 }
